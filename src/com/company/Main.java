64c5492ff9adbebea81f33f18c34ea162588c7be
Niickwan <89967473+Niickwan@users.noreply.github.com>
2021-09-06 12:06:37 +0200

    Calculator

---
+++ src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+        double result = 0.0;
+        boolean exit = false;
+
+        while (exit == false) {
+            System.out.println("Calculator");
+            System.out.print("Enter 1st number: ");
+            double x = scanner.nextDouble();
+            System.out.print("Enter operator (+, -, x, /)");
+            String operator = scanner.next();
+            System.out.print("Enter 2nd number: ");
+            double y = scanner.nextDouble();
+
+            if (operator.equals("+")) {
+                result = x + y;
+                System.out.println(result);
+                System.out.print("Exit? y/n: ");
+                String breakOut = scanner.next();
+                if (breakOut .equals("y") || breakOut.equals("Y")){
+                    exit = true;
+                }
+            }
+            if (operator.equals("-")) {
+                result = x - y;
+                System.out.println(result);
+                System.out.print("Exit? y/n: ");
+                String breakOut = scanner.next();
+                if (breakOut .equals("y") || breakOut.equals("Y")){
+                    exit = true;
+                }
+            }
+            if (operator.equals("x") || operator.equals("*")) {
+                result = x * y;
+                System.out.println(result);
+                System.out.print("Exit? y/n: ");
+                String breakOut = scanner.next();
+                if (breakOut .equals("y") || breakOut.equals("Y")){
+                    exit = true;
+                }
+            }
+            if (operator.equals("/")) {
+                result = x / y;
+                System.out.println(result);
+                System.out.print("Exit? y/n: ");
+                String breakOut = scanner.next();
+                if (breakOut .equals("y") || breakOut.equals("Y")){
+                    exit = true;
+                }
+            }
+        }
+    }
+}
